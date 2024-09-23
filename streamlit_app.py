bfa5d52178a4fedfad49274e5060f4719d9ccc6a
KingHoneyB <sirkinghoneybadger@gmail.com>
2024-09-23 08:23:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+  
+  st.write('**X data**')
+  x = df.drop('species')
+  x
+  
+  st.write('**Y data**')
+  y = df['species']
+  y
