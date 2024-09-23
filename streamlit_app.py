320d5044a82660e12e92997f48c97e5ba41de840
KingHoneyB <sirkinghoneybadger@gmail.com>
2024-09-23 09:08:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.sidebar:
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
           'sex': gender}
-  input_df = pd.DataFrame(data, index=[0])
+  input_df = pd.DataFrame(data, index=[1])
   input_penguins = pd.concat([input_df, x], axis=0)
 
 with st.expander('Input features'):
