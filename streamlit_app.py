4fcbf5d63fc152792f17333c9759731d9322b7b7
KingHoneyB <sirkinghoneybadger@gmail.com>
2024-09-23 08:17:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('KingHB')
 
 st.info('App to build Machine Learning models')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
